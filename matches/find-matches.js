7ca797a20c42c70777571f81ef6e53b0649b3115
beth92 <bethtownsend92@gmail.com>
2018-02-04 16:52:02 -0500

    Improve structure of match function

--- matches/find-matches.js
+++ matches/find-matches.js
@@ -1,31 +1,59 @@
 const fs = require('fs');
 
 const findMatches = (cameras, products) => {
-  const results = [];
-  products.forEach((product, index) => {
-    results.push({
-      product_name: product.product_name,
-      listings: []
-    });
-    cameras.forEach((camera) => {
-      // TODO decide if camera matches product
-      const match = Math.random() < 0.01;
-      if(match) {
-        results[index].listings.push(camera);
-      }
-    });
-
-  });
-  logResults(results);
-};
-
-const logResults = (results) => {
   const fd = fs.openSync('./results.txt', 'w');
-  results.forEach((result) => {
-    fs.appendFileSync(fd, `${JSON.stringify(result)}\n`);
+  products.forEach((product) => {
+    const listings = cameras.filter((camera) => {
+      // run tests
+      return isMatch(camera, product);
+    });
+    const result = {
+      product_name: product.product_name,
+      listings
+    };
+    logResult(result, fd);
   });
   fs.closeSync(fd);
 };
 
+const logResult = (result, fd) => {
+    fs.appendFileSync(fd, `${JSON.stringify(result)}\n`);
+};
+
+const isMatch = (camera, product) => {
+  if(camera.title.toLowerCase().indexOf(product.model.toLowerCase()) !== -1) {
+    if(camera.manufacturer.toLowerCase().indexOf(product.manufacturer.toLowerCase()) !== -1){
+      return true;
+    }
+  }
+  return false;
+};
+
 
 module.exports = {findMatches};
+
+
+
+// const results = [];
+// products.forEach((product, index) => {
+//   results.push({
+//     product_name: product.product_name,
+//     listings: []
+//   });
+//   cameras.forEach((camera) => {
+//     // decide if camera matches product
+//     // if so define match as true
+//     // check if manufacturers match
+//     // check if model is in listing title
+//     if(camera.title.toLowerCase().indexOf(product.model.toLowerCase()) !== -1) {
+//       if(camera.manufacturer.toLowerCase().indexOf(product.manufacturer.tolowerCase()) != -1){
+//         const match = true;
+//       }
+//     }
+//     if(match) {
+//       results[index].listings.push(camera);
+//     }
+//   });
+//
+// });
+// logResults(results);
