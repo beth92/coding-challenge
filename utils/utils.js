7f13d21a17cbb6b7a89882862dfcb33edd3174d7
beth92 <bethtownsend92@gmail.com>
2018-02-07 09:10:48 -0500

    specify encoding for writing to file

--- utils/utils.js
+++ utils/utils.js
@@ -5,18 +5,14 @@ const getInputData = (file) => {
   if(!fs.existsSync(file)){
     return `File not found: ${file}`;
   }
-  return fs.readFileSync(file, 'utf8').split('\n').filter((item) => {
-    return (item !== '');
-  }).map(JSON.parse);
+  return fs.readFileSync(file, 'utf8').split('\n').filter((item) => (item !== '')).map(JSON.parse);
 };
 
 // fn to print array to file using \n delimiter
 const logItems = (items, filename) => {
   const fd = fs.openSync(filename, 'w');
-  const itemsString = items.reduce((accum, item) => {
-    return `${accum}${item}\n`;
-  }, '');
-  fs.appendFileSync(fd, itemsString);
+  const itemsString = items.reduce((accum, item) => `${accum}${item}\n`, '');
+  fs.appendFileSync(fd, itemsString, 'utf8');
   fs.closeSync(fd);
 };
 
