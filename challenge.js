f4749ac1cf565aa01c726936b0996977d1154c24
beth92 <bethtownsend92@gmail.com>
2018-02-07 09:39:26 -0500

    run data analysis by default

--- challenge.js
+++ challenge.js
@@ -1,15 +1,13 @@
 // local imports
 const utils = require('./utils/utils');
+const {dataSummary} = require('./data/data-summary');
 const {findCameras} = require('./cameras/find-cameras');
 const {findMatches} = require('./matches/find-matches');
 
 const listings = utils.getInputData('./listings.txt');
 const products = utils.getInputData('./products.txt');
 
-if(process.argv.includes('-d')) {
-  const {dataSummary} = require('./data/data-summary');
-  dataSummary(products, listings);
-}
+dataSummary(products, listings);
 
 // retrieve listings filtered for items that are likely not cameras
 const cameras = findCameras(listings, './cameras/rejects.txt');
